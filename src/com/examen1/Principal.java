33965ea9682d1e4964d78f93da095300eebe6fb4
daniel-bl <danielinformatica457@gmail.com>
2022-08-03 10:10:53 -0500

    Modificación

--- src/com/examen1/Principal.java
+++ src/com/examen1/Principal.java
@@ -3,7 +3,7 @@ package com.examen1;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo examenes Xideral");
+		System.out.println("Hola mundo examenes Xideral Java");
 
 	}
 
