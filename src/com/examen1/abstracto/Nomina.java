d82b298226f71289abb5d81d1f66f1e0c577fb28
daniel-bl <danielinformatica457@gmail.com>
2022-08-05 12:17:49 -0500

    Correccion toString

--- src/com/examen1/abstracto/Nomina.java
+++ src/com/examen1/abstracto/Nomina.java
@@ -4,19 +4,24 @@ public abstract class Nomina {
 	int salario;
 	
 	public String pagar() {
-		return "Se le ha pagado "+salario+" a los trabajadores de oficina";
+		return "Se le ha pagado "+salario+" a: "+this.toString();
 	}
-	
 	
 	public String aumentarSalario(int aumento) {
 		salario += aumento;
-		String res="Se ha aumentado: "+aumento + " a la nomina de los trabajadores de oficina, salario total: "+salario;
+		String res="Se ha aumentado: "+aumento + " a la nomina de: "+this.toString()+", salario total: "+salario;
 		return res;
 	}
 	
 	public String reducirSalario(int monto) {
 		salario -= monto;
-		String res="Se ha reducido: "+monto + " a la nomina de los trabajadores de oficina, salario total: "+salario;
+		String res="Se ha reducido: "+monto + " a la nomina de: "+this.toString()+", salario total: "+salario;
 		return res;
 	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName();
+	}
+	
 }
