55de73325c5160810ee1b79932eb8f2263012da1
daniel-bl <110208796+daniel-bl@users.noreply.github.com>
2022-08-12 13:21:30 -0500

    Proyecto semana 2

---
+++ src/com/examen2/ProyectoSemana2Empleados/src/main/java/com/luv2code/web/jdbc/Prueba.java
@@ -0,0 +1,43 @@
+/*	Becerril Becerril Daniel
+ * 	Proyecto Sistema de Empleados
+ * 	12/08/2022
+ * 	Prueba para verificar funcionamiento
+ * */
+package com.luv2code.web.jdbc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Prueba
+ */
+@WebServlet("/Prueba")
+public class Prueba extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Prueba() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: PRUEBA").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
